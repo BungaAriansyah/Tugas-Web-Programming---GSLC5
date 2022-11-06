9154f59deba2e3fefd9951eadedb318916e0f284
BungaAriansyah <117589563+BungaAriansyah@users.noreply.github.com>
2022-11-06 23:41:12 +0700

    Add files via upload

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@include('navbar')
+
+@extends('temp')
+
+@section('content')
+    
+<div class="jumbotron">
+    <div class="p-3 nb-2 bg-secondary text-white">
+        
+    <h1 class="display-4">Web Programming</h1>
+    <p class="lead">Buatlah suatu tampilan website menggunakan framework laravel feature blade</p>
+    <hr class="my-4">
+    <p>Berikut adalah tampilan website yang sudah saya buat</p>
+    <p>Bunga Sari Ariansyah - 2440062810 - LG01</p>
+    <p class="lead">
+      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </p>
+    </div>
+  </div>
+
+@endsection
---
+++ storage/framework/views/b7c45ed58b78b4a891790a8324edec28e255dbd0.php
@@ -0,0 +1,22 @@
+<?php echo $__env->make('navbar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+
+
+<?php $__env->startSection('content'); ?>
+    
+<div class="jumbotron">
+    <div class="p-3 nb-2 bg-secondary text-white">
+        
+    <h1 class="display-4">Web Programming</h1>
+    <p class="lead">Buatlah suatu tampilan website menggunakan framework laravel feature blade</p>
+    <hr class="my-4">
+    <p>Berikut adalah tampilan website yang sudah saya buat</p>
+    <p>Bunga Sari Ariansyah - 2440062810 - LG01</p>
+    <p class="lead">
+      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </p>
+    </div>
+  </div>
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('temp', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\BUNGA ARIANSYAH\Semester5\Web Programming\cobacoba\resources\views/home.blade.php ENDPATH**/ ?>
